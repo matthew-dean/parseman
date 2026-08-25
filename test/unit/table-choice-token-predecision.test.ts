70b2d4200bc1f42fc79132c82ce11c377b1bb5cc
Matthew Dean <matthew-dean@users.noreply.github.com>
2026-08-25 13:36:44 -0700

    test(table): run emitted token fixtures in strict mode

--- test/unit/table-choice-token-predecision.test.ts
+++ test/unit/table-choice-token-predecision.test.ts
@@ -27,7 +27,9 @@ function outcome(entry: Entry, input: string) {
 
 function precompiled(prog: TableProgram): TableProgram {
   const emitted = emitAssemblySource(resolveTable(prog), prog, STRICT)
-  const factory = new Function(...EMITTED_PARAMS, emitted.source) as PrecompiledAssembly['factory']
+  const factory = new Function(
+    ...EMITTED_PARAMS, `'use strict';\n${emitted.source}`,
+  ) as PrecompiledAssembly['factory']
   return { ...prog, asm: [{ key: 0, factory, plan: emitted.plan, reached: [...emitted.reached] }] }
 }
 
@@ -45,7 +47,7 @@ function countedPrecompiled(
     source = source.replace(/const r=tp\?_pfTokPacked:([^\n]+)/, 'const r=$1')
   }
   const compiled = new Function(
-    ...EMITTED_PARAMS, 'COUNT_LEX', source,
+    ...EMITTED_PARAMS, 'COUNT_LEX', `'use strict';\n${source}`,
   ) as (...args: unknown[]) => ReturnType<PrecompiledAssembly['factory']>
   const factory = ((...args: Parameters<PrecompiledAssembly['factory']>) =>
     compiled(...args, counter)) as PrecompiledAssembly['factory']
